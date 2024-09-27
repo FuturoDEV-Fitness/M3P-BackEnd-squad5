8d346ea3767e47031ac2b3657a17a04b471456a8
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-09-27 18:38:02 -0300

    :sparkles: feat: Adição de Controller e Service do DashBoard

--- src/controllers/DashBoardController.js
+++ src/controllers/DashBoardController.js
@@ -1,35 +1,72 @@
 const DashBoardService = require("../services/dashboard.service");
 
 class DashBoardController {
-
-
-async listarUsuarios(request, response) {
+  async listarUsuarios(request, response) {
     try {
       const listaUsuarios = await DashBoardService.listarUsuarios();
-      return response.json(listaUsuarios);
+      return response.status(200).json(listaUsuarios);
+      //listarUmUsuario aqui dentro ou fora?
+
+      /*const { idAutenticado } = request.usuarioId;
+      let usuarioLogado = null;
+      if (idAutenticado) {
+        usuarioLogado = await DashBoardService.listarUmUsuario(idAutenticado);
+      }
+      return response.status(200).json({ listaUsuarios, usuarioLogado })*/
     } catch (error) {
       return response
         .status(500)
         .json({ mensagem: "Não foi possível listar usuários" });
     }
   }
-
   async listarUmUsuario(request, response) {
     try {
       const { id } = request.params;
       const { idAutenticado } = request.usuarioId;
-
-      const usuarioBuscado = await DashBoardService.listarUmUsuario(id, idAutenticado);
+      const usuarioBuscado = await DashBoardService.listarUmUsuario(
+        id,
+        idAutenticado
+      );
       if (!usuarioBuscado) {
-        return response
-          .status(400)
-          .json({ mensagem: "Não foi possível exibir o usuário ou não possui permissão" }); //}
+        return response.status(400).json({
+          mensagem: "Não foi possível exibir o usuário ou não está logado",
+        });
       }
       return response.status(200).json(usuarioBuscado); //
     } catch (error) {
       return response.status(500).json({ mensagem: "Erro ao exibir usuário" });
     }
-}
+  }
+
+  async listarLocais(request, response) {
+    try {
+      const { listaLocais, totalLocais } =
+        await DashBoardService.listarLocais();
+      //mesma lógica de exibir usuarios + usuario logado???
+      return response.status(200).json(listaLocais, totalLocais);
+    } catch (error) {
+      return response.status(500).json({ mensagem: "Erro ao exibir locais" });
+    }
+  }
+  async listarLocaisUsuario(request, response) {
+    try {
+      const { idAutenticado } = request.usuarioId;
+      const { locaisIndividuais, totalLocaisIndividual } =
+        await DashBoardService.listarLocaisUsuario(idAutenticado);
+      if (!locaisIndividuais) {
+        return response.status(400).json({
+          mensagem: "Não foi possível exibir locais ou não está logado",
+        });
+      }
+      return response
+        .status(200)
+        .json(locaisIndividuais, totalLocaisIndividual);
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ mensagem: "Erro ao listar locais do usuário" });
+    }
+  }
 }
 
-  module.exports = DashBoardController()
+module.exports = new DashBoardController();
--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -1,5 +1,78 @@
-class DashBoardService {
+const Local = require("../models/Local");
+const Usuario = require("../models/Usuario");
+const { Op } = require("sequelize");
 
+class DashBoardService {
+  async listarUsuarios() {
+    const listaUsuarios = await Usuario.findAll({
+      attributes: ["id", "nome", "email", "sexo"],
+    });
+
+    const totalUsuarios = await Usuario.count();
+    const totalHomens = await Usuario.count({ where: { sexo: "masculino" } });
+    const totalMulheres = await Usuario.count({ where: { sexo: "feminino" } });
+    const totalOutros = await Usuario.count({
+      where: {
+        sexo: {
+          [Op.notIn]: ["masculino", "feminino"],
+        },
+      },
+    });
+
+    return {
+      listaUsuarios,
+      totalUsuarios,
+      totalHomens,
+      totalMulheres,
+      totalOutros,
+    };
+  }
+  async listarUmUsuario(id, idAutenticado) {
+    const usuarioBuscado = await Usuario.findOne(idAutenticado, {
+      //ou findByPk(id)
+      attributes: ["id", "nome", "email", "sexo"],
+    });
+    if (!usuarioBuscado) return null;
+    if (usuarioBuscado.id !== idAutenticado) return null;
+
+    return usuarioBuscado;
+  }
+
+  async listarLocais() {
+    const listaLocais = await Local.findAll({
+      attributes: [
+        "id",
+        "nomeLocal",
+        "descricao",
+        "praticasPermitidas",
+        "latitude",
+        "longitude",
+      ],
+    });
+
+    const totalLocais = await Usuario.count();
+    return { listaLocais, totalLocais };
+  }
+  async listarLocaisUsuario(idAutenticado) {
+    const locaisIndividuais = await Local.findAll({
+        /*where: { id_usuario: idAutenticado},*/ //<--melhor assim?
+      include: [
+        {
+          model: Usuario,
+          where: {
+            id: idAutenticado, //id_usuario
+          },
+        },
+      ],
+    });
+    if(!locaisIndividuais) return null
+    const totalLocaisIndividual = await Local.count({
+        where: {
+          id_usuario: idAutenticado, 
+        },
+      });
+    return {locaisIndividuais, totalLocaisIndividual }
+  }
 }
 
-module.exports = new DashBoardService()
+module.exports = new DashBoardService();
