19febd3138a6f459b29449a972a8420d7d969fac
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-09-30 21:27:51 -0300

    alterações antes de corrigir relacionamentos

--- src/database/seeders/20240805044131-locais_seeder.js
+++ src/database/seeders/20240805044131-locais_seeder.js
@@ -5,128 +5,84 @@ const { obterLocal } = require("../../services/geoFinder");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /*
-      const locais = [
-        {
-          nomeLocal: 'Academia Central',
-          descricao: 'Academia com diversos equipamentos para musculação e aerobica.',
-          localidade: 'Rua das Palmeiras, nº 100',
-          cep: '20271190',
-          praticasPermitidas: 'Musculação, Aeróbica, Yoga',
-          idUsuario: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          deletedAt: null
-        },
-        {
-          nomeLocal: 'Parque das Aves',
-          descricao: 'Parque ao ar livre ideal para caminhadas e esportes ao ar livre.',
-          localidade: 'Avenida das Aves, nº 250',
-          cep: '30672250',
-          praticasPermitidas: 'Caminhada, Corrida, Yoga',
-          idUsuario: 2, 
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          deletedAt: null
-        },
-        {
-          nomeLocal: 'Centro de Treinamento Fitness',
-          descricao: 'Centro de treinamento especializado em esportes de alta performance.',
-          localidade: 'Praça da Vitória, nº 15',
-          cep: '05653070',
-          praticasPermitidas: 'Treinamento funcional, Crossfit',
-          idUsuario: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          deletedAt: null
-        },
-        {
-          nomeLocal: 'Clube de Corrida',
-          descricao: 'Clube dedicado a corridas e eventos de atletismo.',
-          localidade: 'Rua das Flores, nº 55',
-          cep: '40050565',
-          praticasPermitidas: 'Corrida, Atletismo',
-          idUsuario: 4, 
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          deletedAt: null
-        },
-        {
-          nomeLocal: 'Estúdio de Yoga Zen',
-          descricao: 'Estúdio de yoga com aulas para todas as idades e níveis.',
-          localidade: 'Rua da Paz, nº 77',
-          cep: '90810240',
-          praticasPermitidas: 'Yoga, Meditação',
-          idUsuario: 5, 
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          deletedAt: null
-        },
-        {
-          nomeLocal: 'Ginásio de Lutas',
-          descricao: 'Ginásio especializado em artes marciais e esportes de combate.',
-          localidade: 'Rua dos Lutadores, nº 89',
-          cep: '88047401',
-          praticasPermitidas: 'Jiu-Jitsu, Muay Thai, Boxe',
-          idUsuario: 6, 
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          deletedAt: null
-        },
-        {
-          nomeLocal: 'Academia de Dança',
-          descricao: 'Academia dedicada à prática de diversas modalidades de dança.',
-          localidade: 'Avenida das Artes, nº 120',
-          cep: '88070730',
-          praticasPermitidas: 'Dança clássica, Dança moderna, Hip Hop',
-          idUsuario: 7, 
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          deletedAt: null
-        },
-        {
-          nomeLocal: 'Centro de Natação',
-          descricao: 'Centro especializado em natação para todas as idades.',
-          localidade: 'Rua dos Nadadores, nº 200',
-          cep: '80250070',
-          praticasPermitidas: 'Natação',
-          idUsuario: 8, 
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          deletedAt: null
-        },
-        {
-          nomeLocal: 'Centro de Fitness',
-          descricao: 'Centro de fitness com diversas opções de atividades físicas.',
-          localidade: 'Rua do Esporte, nº 300',
-          cep: '05001200',
-          praticasPermitidas: 'Musculação, Cardio',
-          idUsuario: 9, 
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          deletedAt: null
-        },
-        {
-          nomeLocal: 'Academia Personal',
-          descricao: 'Academia com foco em treino personalizado.',
-          localidade: 'Rua dos Treinadores, nº 400',
-          cep: '11075501',
-          praticasPermitidas: 'Treino Personalizado, Musculação',
-          idUsuario: 10, 
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          deletedAt: null
-        }
-      ];
+    
+     
+  
+      // // Atualizar latitude e longitude para cada local
+      // for (let local of locais) {
+      //   const { lat, lng } = await obterLocal(local.cep);
+      //   local.latitude = lat;
+      //   local.longitude = lng;
+      // }
 
-      // Atualizar latitude e longitude para cada local
-      for (let local of locais) {
-        const { lat, lng } = await obterLocal(local.cep);
-        local.latitude = lat;
-        local.longitude = lng;
-      }
-*/
-    await queryInterface.bulkInsert("locais", locais, {});
+    //await 
+    return queryInterface.bulkInsert("locais", [
+      {
+        nomeLocal: 'Academia Central',
+        descricaoLocal: 'Academia com diversos equipamentos para musculação e aeróbica.',
+        cep: '20271190',
+        endereco: 'Rua das Palmeiras, nº 100',
+        bairro: 'Centro',
+        cidade: 'Rio de Janeiro',
+        estado: 'RJ',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+        id_usuario: 1 
+      },
+      {
+        nomeLocal: 'Parque das Aves',
+        descricaoLocal: 'Parque ao ar livre ideal para caminhadas e esportes ao ar livre.',
+        cep: '30672250',
+        endereco: 'Avenida das Aves, nº 250',
+        bairro: 'Aves',
+        cidade: 'Belo Horizonte',
+        estado: 'MG',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+        id_usuario: 2 
+      },
+      {
+        nomeLocal: 'Centro de Treinamento Fitness',
+        descricaoLocal: 'Centro de treinamento especializado em esportes de alta performance.',
+        cep: '05653070',
+        endereco: 'Praça da Vitória, nº 15',
+        bairro: 'Vitória',
+        cidade: 'São Paulo',
+        estado: 'SP',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+        id_usuario: 3 
+      },
+      {
+        nomeLocal: 'Clube de Corrida',
+        descricaoLocal: 'Clube dedicado a corridas e eventos de atletismo.',
+        cep: '40050565',
+        endereco: 'Rua das Flores, nº 55',
+        bairro: 'Flora',
+        cidade: 'Salvador',
+        estado: 'BA',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+        id_usuario: 4 
+      },
+      {
+        nomeLocal: 'Estúdio de Yoga Zen',
+        descricaoLocal: 'Estúdio de yoga com aulas para todas as idades e níveis.',
+        cep: '90810240',
+        endereco: 'Rua da Paz, nº 77',
+        bairro: 'Paz',
+        cidade: 'Porto Alegre',
+        estado: 'RS',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+        id_usuario: 5 
+      },
+    ]);
   },
 
   async down(queryInterface, Sequelize) {
