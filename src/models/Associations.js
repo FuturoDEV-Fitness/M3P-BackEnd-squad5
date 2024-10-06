8266e7a847875e61d41117e562624d391b954151
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-10-05 22:04:41 -0300

    relations apenas

--- src/models/Associations.js
+++ src/models/Associations.js
@@ -1,14 +1,25 @@
 const Usuario = require('./Usuario');
 const Local = require('./Local');
+const Endereco = require('./Local');
 
 
 Usuario.hasMany(Local, {
-  foreignKey: 'idUsuario'
+  foreignKey: 'id_usuario'
    
 });
 
 Local.belongsTo(Usuario, {
-  foreignKey: 'idUsuario'
+  foreignKey: 'id_usuario'
+   
+});
+
+Endereco.belongsTo(Usuario, {
+  foreignKey: "id_usuario",
+  as: "usuario",
+});
+
+Usuario.hasOne(Endereco, {
+  foreignKey: 'id_usuario'
    
 });
 
